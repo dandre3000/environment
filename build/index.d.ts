a2865a81fa1ba7a183d4de380e72c2d20959210e
dandre3000 <deaundre.payne@gmail.com>
2026-05-29 14:03:00 -0400

    Update index.d.ts

--- build/index.d.ts
+++ build/index.d.ts
@@ -6,3 +6,39 @@ declare module '@dandre3000/environment' {
     /** The name of the current runtme. */
     export const runtime: 'bun' | 'browser' | 'deno' | 'node'
 }
+
+declare module '@dandre3000/environment/browser.js' {
+    /** Alias for process.exit or globalThis.close. */
+    export const exit: () => never
+    /** True if the current global scope is the main thread or false otherwise. */
+    export const isMainThread: boolean
+    /** The name of the current runtme. */
+    export const runtime: 'browser'
+}
+
+declare module '@dandre3000/environment/bun.js' {
+    /** Alias for process.exit or globalThis.close. */
+    export const exit: (code?: number | string | null) => never
+    /** True if the current global scope is the main thread or false otherwise. */
+    export const isMainThread: boolean
+    /** The name of the current runtme. */
+    export const runtime: 'bun'
+}
+
+declare module '@dandre3000/environment/deno.js' {
+    /** Alias for process.exit or globalThis.close. */
+    export const exit: (code?: number | string | null) => never
+    /** True if the current global scope is the main thread or false otherwise. */
+    export const isMainThread: boolean
+    /** The name of the current runtme. */
+    export const runtime: 'deno'
+}
+
+declare module '@dandre3000/environment/node.js' {
+    /** Alias for process.exit or globalThis.close. */
+    export const exit: (code?: number | string | null) => never
+    /** True if the current global scope is the main thread or false otherwise. */
+    export const isMainThread: boolean
+    /** The name of the current runtme. */
+    export const runtime: 'node'
+}
